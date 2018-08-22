716d9b57d4b7086c73f322aec6bd90fdd5f70cb1
Evan Truesdale <kayakcat@truecrew.net>
2018-08-21 21:32:42 -0700

    Restructured menu scripts, speed sliders track with variable, menu open and close

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
+
+public class MenuActions : MonoBehaviour
+{
+
+    public void CloseMainMenu()
+    {
+        GameObject[] Menus = GameObject.FindGameObjectsWithTag("MainMenu");
+        foreach (GameObject Menu in Menus)
+        {
+            Destroy(Menu);
+        }
+        MenuController.SetMenuState(false);
+    }
+
+    public void ChangeSpeed()
+    {
+        float newSpeed = gameObject.GetComponent<Slider>().value;
+        MenuController.SetSpeed(newSpeed);
+        GameObject[] Sliders = GameObject.FindGameObjectsWithTag("SpeedSlider");
+        foreach (GameObject Slider in Sliders)
+        {
+            Slider.GetComponent<Slider>().value = newSpeed;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+
+    public class MenuController : MonoBehaviour
+    {
+
+        static Transform PlayerTransform;
+        static GameObject MainMeunPrefab;
+        static bool isMenuOpen;
+        static float distance = 15;
+        static float speed = 1;
+
+        void Start()
+        {
+            PlayerTransform = GameObject.Find("Player").transform;
+            MainMeunPrefab = Resources.Load<GameObject>("MainMenuPrefab");
+            isMenuOpen = false;
+        }
+
+        void Update()
+        {
+            if (Input.GetButton("Fire1") && !isMenuOpen)
+            {
+                OpenMainMenu();
+            }
+        }
+
+        public void OpenMainMenu()
+        {
+            isMenuOpen = true;
+
+            Vector3 menuPosition = PlayerTransform.position + PlayerTransform.forward * distance;
+            Instantiate(MainMeunPrefab, menuPosition, Quaternion.Euler(0, 90, 0));
+
+            menuPosition = PlayerTransform.position + PlayerTransform.forward * -distance;
+            Instantiate(MainMeunPrefab, menuPosition, Quaternion.Euler(0, -90, 0));
+
+            GameObject[] SpeedSliders = GameObject.FindGameObjectsWithTag("SpeedSlider");
+            foreach (GameObject Slider in SpeedSliders)
+            {
+                Slider.GetComponent<Slider>().value = speed;
+            }
+        }
+
+        public static void SetMenuState(bool input)
+        {
+            isMenuOpen = input;
+        }
+
+        public static void SetSpeed(float input){
+            speed = input;
+        }
+    }
+}
