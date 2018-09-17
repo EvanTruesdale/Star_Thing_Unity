387e88473263766f6e4037cf2fdf33fe0061013d
Evan Truesdale <kayakcat@truecrew.net>
2018-09-17 11:41:28 -0700

    Fixed Rotation Period bug

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -17,6 +17,6 @@ public class Rotation : MonoBehaviour
     void FixedUpdate()
     {
         //Rotate planet around y axis, which is tilted with respect to world space
-        gameObject.transform.Rotate(0, -1 * Time.fixedDeltaTime / BodyInitialzation.GetRotationPeriod(gameObject.name) * 3600, 0);
+        gameObject.transform.Rotate(0, -Mathf.PI * Time.fixedDeltaTime / BodyInitialzation.GetRotationPeriod(gameObject.name) * 3600, 0);
     }
 }
