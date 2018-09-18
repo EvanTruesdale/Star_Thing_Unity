f14d19de13a4962ed13ab391e15680c6de36eaec
Evan Truesdale <kayakcat@truecrew.net>
2018-09-18 13:56:43 -0700

    Fixed trail distance errors

--- Assets/Scripts/TrailActions.cs
+++ Assets/Scripts/TrailActions.cs
@@ -6,6 +6,6 @@ public class TrailActions : MonoBehaviour {
 	
     //Change trail time based on Speed (UPDATE)
 	void Update () {
-        GetComponent<TrailRenderer>().time = 15f / GetComponentInParent<Rigidbody>().velocity.magnitude;
+        GetComponent<TrailRenderer>().time = 5f / Mathf.Pow(GetComponentInParent<Rigidbody>().velocity.magnitude, 3f);
 	}
 }
