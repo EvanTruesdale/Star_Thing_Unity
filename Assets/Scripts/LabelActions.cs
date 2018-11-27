1ef9f585e3f0d1ef2991318b93240cc81ee5a390
Evan Truesdale <kayakcat@truecrew.net>
2018-11-27 07:53:02 -0800

    Add labels for bodies

---
+++ Assets/Scripts/LabelActions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<TextMesh>().text = GetComponent<Transform>().parent.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 PS = GetComponent<Transform>().parent.localScale;
+        GetComponent<Transform>().localScale = new Vector3(.3f/PS.x, .3f/PS.y, .3f/PS.z);
+
+        GameObject Camera = GameObject.Find("Main Camera");
+        Vector3 lookAt = (GetComponent<Transform>().position - Camera.GetComponent<Transform>().position) * 2;
+        GetComponent<Transform>().LookAt(lookAt);
+	}
+}
